76fbd91abff633c52987aeedf993bd543bbcd6e8
Viggo Wozniak <vmpwozniak@gmail.com>
2024-10-02 21:08:23 -0600

    Updated fetch stage TB

--- riscvpipelined.srcs/sim_1/new/fetch_TB.sv
+++ riscvpipelined.srcs/sim_1/new/fetch_TB.sv
@@ -24,11 +24,10 @@ module fetch_TB();
     
     //Signals to apply stimulus and check result
     logic clk, reset, PCSrcE, StallF;
-    logic [31:0] PCTargetE, InstrF, PCF, PCPlus4F;
+    logic [31:0] PCTargetE, PCF, PCPlus4F;
     
-    logic [31:0] TestInstr [127:0];
     
-    fetchstage DUT(clk, reset, PCTargetE, PCSrcE, StallF, InstrF, PCF, PCPlus4F);
+    Fstage DUT(clk, reset, PCTargetE, PCSrcE, StallF, PCF, PCPlus4F);
     
     always begin
         clk = ~clk; #5;
@@ -39,12 +38,11 @@ module fetch_TB();
         
         //Initialize the system
         clk = 0; reset = 1; PCSrcE = 0; StallF = 0; PCTargetE = 0;
-        $readmemh("riscvprogram_6.txt", TestInstr); 
         #1; reset = 0; #1;
         
         //Check to ensure that base reading from memory, calculation of PC, and PCPlus 4 works correctly
         for (int i = 0; i < 32; i++) begin
-            assert (InstrF === TestInstr[i] & PCF === (i*4) & PCPlus4F === (i*4 + 4) ) else $fatal("Error: Base functionality");
+            assert (PCF === (i*4) & PCPlus4F === (i*4 + 4) ) else $fatal("Error: Base functionality");
             #10;
         end
         
@@ -52,7 +50,7 @@ module fetch_TB();
         //Check to ensure that stalling works correctly:
         //Note currently on instruction 32
         for (int i = 0; i < 32; i++) begin
-             assert (InstrF === TestInstr[32] & PCF === (128) & PCPlus4F === 132) else $fatal("Error: Stall Error");
+             assert (PCF === (128) & PCPlus4F === 132) else $fatal("Error: Stall Error");
              #10;
         end
         
@@ -60,14 +58,12 @@ module fetch_TB();
         //Check to ensure jumping results in correct output
         for (int i = 0; i < 32; i++) begin
             PCSrcE = 0;
-            assert (InstrF === TestInstr[i] & PCF === (i*4) & PCPlus4F === (i*4 + 4) ) else $fatal("Error: Branch Error");
+            assert (PCF === (i*4) & PCPlus4F === (i*4 + 4) ) else $fatal("Error: Branch Error");
             #10;
         end
         
         $display("Simulation Succesful!");
         
-        
-    
     end
     
 
